4eced8f1bd906a132146cc76f1de7e92f7a21da0
karenakim <hxjin@hotmail.com>
2015-06-09 11:53:30 -0400

    P2- playback controls done except prev/next; need to add master/detail

--- app/src/main/java/com/nano/karen/SpotifyStreamer/PlaybackActivity.java
+++ app/src/main/java/com/nano/karen/SpotifyStreamer/PlaybackActivity.java
@@ -1,6 +1,7 @@
 package com.nano.karen.SpotifyStreamer;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.media.MediaMetadata;
 import android.media.MediaPlayer;
@@ -9,6 +10,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -17,7 +19,12 @@ import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 
+import static android.view.View.VISIBLE;
+
 public class PlaybackActivity extends ActionBarActivity {
+
+    MediaPlayer mediaPlayer;
+
     private TextView artistNameView;
     private TextView albumNameView;
     private ImageView albumImageView;
@@ -25,6 +32,11 @@ public class PlaybackActivity extends ActionBarActivity {
     private TextView mStart;
     private TextView mEnd;
     private SeekBar mSeekbar;
+    private ImageView mSkipPrev;
+    private ImageView mSkipNext;
+    private ImageView mPlayPause;
+    private Drawable mPauseDrawable;
+    private Drawable mPlayDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +46,8 @@ public class PlaybackActivity extends ActionBarActivity {
         Intent intent = getIntent();
         TrackListItem mTrack = intent.getExtras().getParcelable("my parcel");
 
+        mediaPlayer = new MediaPlayer();
+
         artistNameView = (TextView) findViewById(R.id.playback_artist_name);
         albumNameView = (TextView) findViewById(R.id.playback_album_name);
         albumImageView = (ImageView) findViewById(R.id.playback_album_image);
@@ -41,12 +55,16 @@ public class PlaybackActivity extends ActionBarActivity {
         mStart = (TextView) findViewById(R.id.startText);
         mEnd = (TextView) findViewById(R.id.endText);
         mSeekbar = (SeekBar) findViewById(R.id.seekBar);
+        mPlayPause = (ImageView) findViewById(R.id.playback_play_pause);
+        mSkipNext = (ImageView) findViewById(R.id.playback_next);
+        mSkipPrev = (ImageView) findViewById(R.id.playback_prev);
+
+        mPauseDrawable = getDrawable(android.R.drawable.ic_media_pause);
+        mPlayDrawable = getDrawable(android.R.drawable.ic_media_play);
 
         artistNameView.setText(mTrack.artistName);
         trackNameView.setText(mTrack.trackName);
         albumNameView.setText(mTrack.albumName);
-        mStart.setText("0");
-        mEnd.setText("100");
 
 
         if (!mTrack.trackImageURL.equals("")) {
@@ -62,7 +80,6 @@ public class PlaybackActivity extends ActionBarActivity {
                     .into(albumImageView);
         }
 
-        MediaPlayer mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
             //mediaPlayer.setDataSource(trackURL);
@@ -77,27 +94,54 @@ public class PlaybackActivity extends ActionBarActivity {
             e.printStackTrace();
             Log.e("Playback", "bad stream");
         }
+
+        int duration = mediaPlayer.getDuration();
+        mSeekbar.setMax(duration);
+        mStart.setText("0.00");
+        mEnd.setText(String.format("%.2f", duration / 100000.0));
+        mSeekbar.setProgress(mediaPlayer.getCurrentPosition());
+        mSeekbar.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        mSeekbar.setProgress(mediaPlayer.getCurrentPosition());
+                        mSeekbar.postDelayed(this, 500);
+                    }
+                }, 1000);
         mediaPlayer.start();
 
-/*
+
         mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mStart.setText(Utils.formatMillis(progress));
+                if (fromUser)
+                    mediaPlayer.seekTo(progress);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                stopSeekbarUpdate();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                getMediaController().getTransportControls().seekTo(seekBar.getProgress());
-                scheduleSeekbarUpdate();
             }
-        }); */
+        });
 
+        mPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    mPlayPause.setVisibility(VISIBLE);
+                    mPlayPause.setImageDrawable(mPlayDrawable);
+                } else {
+                    mediaPlayer.start();
+                    mPlayPause.setVisibility(VISIBLE);
+                    mPlayPause.setImageDrawable(mPauseDrawable);
+
+                }
+            }
+        });
     }
 
     @Override
@@ -118,19 +162,6 @@ public class PlaybackActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-    private void updateDuration(MediaMetadata metadata) {
-        if (metadata == null) {
-            return;
-        }
-        int duration = (int) metadata.getLong(MediaMetadata.METADATA_KEY_DURATION);
-        mSeekbar.setMax(duration);
-        mEnd.setText(duration);
-    }
-
-
-
 }
